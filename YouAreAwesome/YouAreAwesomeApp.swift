bf164dd51813c0da1b91438a9585b4cf2faeb08f
Terence-PDX <61093911+Terence-PDX@users.noreply.github.com>
2025-12-23 09:34:40 -0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by TDodge47 on 2025-12-23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
